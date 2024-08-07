947c3f84dd9a074b6dc5c621e645dbdb6094e138
shezdawg <stuff@shezdawg.com>
2024-08-07 22:41:12 +1000

    make stdin cmds execute as they are entered

--- ToyRobotApp/ToyRobotApp/Program.cs
+++ ToyRobotApp/ToyRobotApp/Program.cs
@@ -8,23 +8,24 @@ namespace ToyRobotApp
         static void Main(string[] args)
         {
             Robot robot = new Robot();
+
             if (args.Length > 0)
             {
-                // Read commands from a file
+                // Process commands from a file
                 var lines = File.ReadAllLines(args[0]);
                 ProcessCommands(robot, lines);
+                Console.WriteLine("Press Enter to exit...");
+                Console.ReadLine();
             }
             else
             {
-                // Read commands from standard input
+                // Process commands from standard input
                 Console.WriteLine("Enter commands (type 'END' to finish):");
-                var input = new System.Collections.Generic.List<string>();
-                string line;
-                while ((line = Console.ReadLine()) != "END")
+                string command;
+                while ((command = Console.ReadLine()) != "END")
                 {
-                    input.Add(line);
+                    ProcessCommand(robot, command);
                 }
-                ProcessCommands(robot, input.ToArray());
             }
         }
 
@@ -32,39 +33,44 @@ namespace ToyRobotApp
         {
             foreach (var command in commands)
             {
-                if (command.StartsWith("PLACE"))
+                ProcessCommand(robot, command);
+            }
+        }
+
+        private static void ProcessCommand(Robot robot, string command)
+        {
+            if (command.StartsWith("PLACE"))
+            {
+                var parts = command.Split(new[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+                if (parts.Length == 4 && int.TryParse(parts[1], out int x) && int.TryParse(parts[2], out int y))
                 {
-                    var parts = command.Split(new[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
-                    if (parts.Length == 4 && int.TryParse(parts[1], out int x) && int.TryParse(parts[2], out int y))
+                    try
                     {
-                        try
-                        {
-                            robot.Place(x, y, parts[3]);
-                        }
-                        catch (ArgumentException ex)
-                        {
-                            Console.WriteLine(ex.Message);
-                        }
+                        robot.Place(x, y, parts[3]);
+                    }
+                    catch (ArgumentException ex)
+                    {
+                        Console.WriteLine(ex.Message);
                     }
                 }
-                else if (robot.IsPlaced)
+            }
+            else if (robot.IsPlaced)
+            {
+                switch (command)
                 {
-                    switch (command)
-                    {
-                        case "MOVE":
-                            robot.Move();
-                            break;
-                        case "LEFT":
-                            robot.Left();
-                            break;
-                        case "RIGHT":
-                            robot.Right();
-                            break;
-                        case "REPORT":
-                            var (x, y, f) = robot.Report();
-                            Console.WriteLine($"{x},{y},{f}");
-                            break;
-                    }
+                    case "MOVE":
+                        robot.Move();
+                        break;
+                    case "LEFT":
+                        robot.Left();
+                        break;
+                    case "RIGHT":
+                        robot.Right();
+                        break;
+                    case "REPORT":
+                        var (x, y, f) = robot.Report();
+                        Console.WriteLine($"{x},{y},{f}");
+                        break;
                 }
             }
         }
