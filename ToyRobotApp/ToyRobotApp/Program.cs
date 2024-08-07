8a16ef234cf7900a6a6990838eaf995fb508cda9
shezdawg <stuff@shezdawg.com>
2024-08-07 21:52:52 +1000

    create robot commands

--- ToyRobotApp/ToyRobotApp/Program.cs
+++ ToyRobotApp/ToyRobotApp/Program.cs
@@ -10,7 +10,25 @@ namespace ToyRobotApp
     {
         static void Main(string[] args)
         {
-            
+            Robot robot = new Robot();
+            robot.Place(0, 0, "NORTH");
+            Console.WriteLine($"Initial Position: {robot.Report()}");
+
+            robot.Move();
+            Console.WriteLine($"After Move: {robot.Report()}");
+
+            robot.Left();
+            Console.WriteLine($"After Left Turn: {robot.Report()}");
+
+            robot.Move();
+            Console.WriteLine($"After Move: {robot.Report()}");
+
+            robot.Right();
+            Console.WriteLine($"After Right Turn: {robot.Report()}");
+
+            robot.Move();
+            Console.WriteLine($"After Move: {robot.Report()}");
+
         }
     }
 }
